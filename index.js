9a4036fcdc2c48badcf2369e94f3c70c650ffd9e
mumsuz <mumsuz@outlook.com>
2025-03-22 19:42:40 +0300

    update

---
+++ index.js
@@ -0,0 +1,7 @@
+@keyframes kaydir {
+    0%   { transform: translateY(0); }
+    33%  { transform: translateY(-50%); }
+    66%  { transform: translateY(-100%); }
+    100% { transform: translateY(0); }
+  }
+  
