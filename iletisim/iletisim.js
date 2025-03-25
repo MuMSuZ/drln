97684db70818fe8b9697b39ed6a97806ae3b8c21
mumsuz <mumsuz@outlook.com>
2025-03-25 14:38:02 +0300

    add and update

---
+++ iletisim/iletisim.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
+  }
+  
+
+  document.getElementById("iletisimFormu").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Mesajınız başarıyla gönderildi. En kısa sürede sizinle iletişime geçilecektir.");
+    this.reset();
+  });
+  
